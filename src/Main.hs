1e011abd0746d2f24176c95284bad612efe33514
maciek <macieq2048@gmail.com>
2021-10-20 16:17:23 +0200

    add desc

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,11 @@
+-- Dla podanej liczby n podaj, jeśli to możliwe, trójkę pitagorejską a, b, c taką, że a + b + c = n. Dla n =
+-- 12 jest nią odpowiednio: (3, 4, 5) ponieważ 32 + 42 = 5
+-- 2
+-- . Dodatkowo należy narzucić ograniczenie,
+-- aby generować tylko pierwotne trójki pitagorejskie. Przykładowo (6, 8, 10) nie jest pierwotną trójką
+-- pitagorejską bo wszystkie jej wartości mają wspólny dzielnik równy 2. Jeśli to niemożliwe podaj
+-- trójkę pitagorejską dla największego możliwego m gdzie m < n.
+
 import Data.Char (generalCategory)
 import Data.List ()
 import Text.ParserCombinators.ReadPrec (reset)
