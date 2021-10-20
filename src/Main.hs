f2a8c6249383d30589fb3d240036166c13596b3a
maciek <macieq2048@gmail.com>
2021-10-20 17:41:09 +0200

    update desc

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,5 @@
 -- zbiór A składający się z dwóch rodzajów elementów x i y o mocy |A| = |x|+|y|=m gdzie |x| ≥ |y|.
 -- Prawdopodobieństwo wylosowania dwóch elementów x ze zbioru A powinno być równe ½.
 
--- 4 Kontrliczba to liczba powstała przez odwrócenie kolejności cyfr. Dla 3476576 kontrliczbą będzie 6756743. Dla liczb
--- kończących jednym bądź większą liczbą zer kontrliczba będzie ich pozbawiona na początku. Kontrliczbą 138000
--- będzie 831.
 -- Przykładowo dla n=20, m=21, |x| = 15 a |y| = 6, prawdopodobieństwo wylosowania dwóch
 -- elementów x = P(xx) = (15/21)*(14/20) = 1/2.
